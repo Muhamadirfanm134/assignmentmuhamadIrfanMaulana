fd8c6fe9a5e87801cb9b2796bc15b57d275dda94
Muhamad Irfan Maulana™ <muhamadirfan45513@gmail.com>
2021-08-12 16:36:47 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,396 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Muhamad Irfan Maulana</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="Themesbrand" name="author" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}">
+
+    <!-- Bootstrap Css -->
+    <link href="{{ asset('css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+    <!-- Icons Css -->
+    <link href="{{ asset('css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+    <!-- App Css-->
+    <link href="{{ asset('css/app.css') }}" id=" app-style" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body data-layout="detached" data-topbar="colored">
+
+    <!-- <body data-layout="horizontal" data-topbar="dark"> -->
+
+    <div class="container-fluid">
+        <!-- Begin page -->
+        <div id="layout-wrapper">
+
+            <div class="vertical-menu">
+
+                <div class="">
+                    <!--- Sidemenu -->
+                    <div id="sidebar-menu">
+                        <!-- Left Menu Start -->
+                        <ul class="metismenu list-unstyled" id="side-menu">
+                            <li class="menu-title">Person List</li>
+                            <div class="accordion" id="accordionExample">
+                                <div class="card-header1">
+                                    <p class="btn text-left" style="color:white;">
+                                        <i class="fas fa-user-circle"></i> ACTOR
+                                    </p>
+                                </div>
+
+                                <div class="card-header" id="headingOne">
+                                    <p class="btn mb-0" type="button" data-toggle="collapse" data-target="#collapseOne"
+                                        aria-expanded="true" aria-controls="collapseOne"><i class="fas fa-flag-usa"></i>
+                                        International
+                                    </p>
+                                </div>
+
+                                <div id="collapseOne" class="collapse " aria-labelledby="headingOne"
+                                    data-parent="#accordionExample">
+
+                                    <div class="list-group">
+                                        <a class="list-group-item list-item" id="btn1">
+                                            <p>Tom Cruise</p>
+                                        </a>
+                                        <a class="list-group-item list-item" id="btn2">
+                                            <p>Johny Depp</p>
+                                        </a>
+                                    </div>
+                                </div>
+
+                                <div class="card-header" id="headingTwo">
+                                    <p class="btn mb-0" type="button" data-toggle="collapse" data-target="#collapseTwo"
+                                        aria-expanded="true" aria-controls="collapseTwo"><i class="fas fa-flag"></i>
+                                        Indonesia
+                                    </p>
+                                </div>
+
+                                <div id="collapseTwo" class="collapse " aria-labelledby="headingTwo"
+                                    data-parent="#accordionExample">
+
+                                    <div class="list-group">
+                                        <a class="list-group-item list-item" id="btn3">
+                                            <p>Raditya Dika</p>
+                                        </a>
+                                        <a class="list-group-item list-item" id="btn4">
+                                            <p>Reza Rahardian</p>
+                                        </a>
+                                    </div>
+                                </div>
+
+                                <!-- SCIENTIST -->
+                                <div class="card-header1">
+                                    <p class="btn text-left" style="color:white;">
+                                        <i class="fas fa-flask"></i> SCIENTIST
+                                    </p>
+                                </div>
+
+                                <div class="card-header" id="headingThree">
+                                    <p class="btn mb-0" type="button" data-toggle="collapse"
+                                        data-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+                                        <i class="fas fa-flag-usa"></i>
+                                        International
+                                    </p>
+                                </div>
+
+                                <div id="collapseThree" class="collapse " aria-labelledby="headingThree"
+                                    data-parent="#accordionExample">
+
+                                    <div class="list-group">
+                                        <a class="list-group-item list-item" id="btn5">
+                                            <p>Issac Newton</p>
+                                        </a>
+                                        <a class="list-group-item list-item" id="btn6">
+                                            <p>Albert Einstein</p>
+                                        </a>
+                                    </div>
+                                </div>
+
+                                <div class="card-header" id="headingFour">
+                                    <p class="btn mb-0" type="button" data-toggle="collapse" data-target="#collapseFour"
+                                        aria-expanded="true" aria-controls="collapseFour"><i class="fas fa-flag"></i>
+                                        Indonesia
+                                    </p>
+                                </div>
+
+                                <div id="collapseFour" class="collapse " aria-labelledby="headingFour"
+                                    data-parent="#accordionExample">
+
+                                    <div class="list-group">
+                                        <a class="list-group-item list-item" id="btn7">
+                                            <p>Mulyoto Pangestu</p>
+                                        </a>
+                                        <a class="list-group-item list-item" id="btn8">
+                                            <p>Warsito Taruno</p>
+                                        </a>
+                                    </div>
+                                </div>
+
+                                <!-- POLITICIAN -->
+                                <div class="card-header1">
+                                    <p class="btn text-left" style="color:white;">
+                                        <i class="fas fa-user-tie"></i> POLITICIAN
+                                    </p>
+                                </div>
+
+                                <div class="card-header" id="headingFive">
+                                    <p class="btn mb-0" type="button" data-toggle="collapse" data-target="#collapseFive"
+                                        aria-expanded="true" aria-controls="collapseFive">
+                                        <i class="fas fa-flag-usa"></i>
+                                        International
+                                    </p>
+                                </div>
+
+                                <div id="collapseFive" class="collapse " aria-labelledby="headingFive"
+                                    data-parent="#accordionExample">
+
+                                    <div class="list-group">
+                                        <a class="list-group-item list-item" id="btn9">
+                                            <p>Barack Obama</p>
+                                        </a>
+                                        <a class="list-group-item list-item" id="btn10">
+                                            <p>Donal Trump</p>
+                                        </a>
+                                    </div>
+                                </div>
+
+                                <div class="card-header" id="headingSix">
+                                    <p class="btn mb-0" type="button" data-toggle="collapse" data-target="#collapseSix"
+                                        aria-expanded="true" aria-controls="collapseSix"><i class="fas fa-flag"></i>
+                                        Indonesia
+                                    </p>
+                                </div>
+
+                                <div id="collapseSix" class="collapse " aria-labelledby="headingSix"
+                                    data-parent="#accordionExample">
+
+                                    <div class="list-group">
+                                        <a class="list-group-item list-item" id="btn11">
+                                            <p>Megawati Soekarnoputri</p>
+                                        </a>
+                                        <a class="list-group-item list-item" id="btn12">
+                                            <p>Fadlizon</p>
+                                        </a>
+                                    </div>
+                                </div>
+
+
+                            </div>
+                        </ul>
+
+                    </div>
+                    <!-- Sidebar -->
+                </div>
+            </div>
+            <!-- Left Sidebar End -->
+
+            <!-- ============================================================== -->
+            <!-- Start right Content here -->
+            <!-- ============================================================== -->
+            <div class="main-content">
+
+                <div class="page-content">
+
+                    <!-- start page title -->
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="page-title-box d-flex align-items-center justify-content-between">
+                                <h4 class="page-title mb-0 font-size-18">Muhamad Irfan Maulana</h4>
+
+                            </div>
+                        </div>
+                    </div>
+                    <!-- end page title -->
+
+                    <div class="row">
+                        <div class="col-xl-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h4 class="card-title mb-4">Search Result</h4>
+
+                                    <div id="form1" style="display:none">
+                                        <form>
+                                            <table class="table table-hover">
+                                                <thead>
+                                                    <tr>
+                                                        <th scope="col">No</th>
+                                                        <th scope="col">Title</th>
+                                                        <th scope="col">Link</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr id="link1">
+                                                        <th scope="row">1</th>
+                                                        <td>Tom Cruise</td>
+                                                        <td>https://en.wikipedia.org/wiki/Tom_Cruise</td>
+                                                    </tr>
+                                                    <tr id="link2">
+                                                        <th scope="row">2</th>
+                                                        <td>Official Tom Cruise Website</td>
+                                                        <td>http://www.tomcruise.com/</td>
+                                                    </tr>
+                                                    <tr id="link3">
+                                                        <th scope="row">3</th>
+                                                        <td>Tom Cruise (@TomCruise) | Twitter</td>
+                                                        <td>https://twitter.com/tomcruise</td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </form>
+                                    </div>
+
+                                    <div id="form2" style="display:none">
+                                        <form>
+                                            <table class="table table-hover">
+                                                <thead>
+                                                    <tr>
+                                                        <th scope="col">No</th>
+                                                        <th scope="col">Title</th>
+                                                        <th scope="col">Link</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr id="link4">
+                                                        <th scope="row">1</th>
+                                                        <td>Johnny Depp</td>
+                                                        <td>https://en.wikipedia.org/wiki/Johnny_Depp</td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </form>
+                                    </div>
+
+                                    <div id="line-chart" class="apex-charts"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- end row -->
+
+                    <div class="row">
+                        <div class="col-xl-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h4 class="card-title mb-4">Content</h4>
+
+                                    <div id="cont1" style="display:none">
+                                        <iframe src="https://en.wikipedia.org/wiki/Tom_Cruise" height="500px"
+                                            width="1300px"></iframe>
+                                    </div>
+
+                                    <div id="cont2" style="display:none">
+                                        <form>
+                                            <iframe src="http://www.tomcruise.com/" height="500px"
+                                                width="1300px"></iframe>
+                                        </form>
+                                    </div>
+
+                                    <div id="cont3" style="display:none">
+                                        <form>
+                                            <iframe src="https://twitter.com/tomcruise" height="500px"
+                                                width="1300px"></iframe>
+                                        </form>
+                                    </div>
+
+                                    <div id="cont4" style="display:none">
+                                        <form>
+                                            <iframe src="https://en.wikipedia.org/wiki/Johnny_Depp" height="500px"
+                                                width="1300px"></iframe>
+                                        </form>
+                                    </div>
+
+                                    <div id="line-chart" class="apex-charts"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- end row -->
+
+
+
+
+                </div>
+                <!-- End Page-content -->
+
+                <footer class="footer">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <script>
+                                document.write(new Date().getFullYear())
+                                </script> © Muhamad Irfan Maulana.
+                            </div>
+
+                        </div>
+                    </div>
+                </footer>
+            </div>
+            <!-- end main content-->
+
+        </div>
+        <!-- END layout-wrapper -->
+
+    </div>
+    <!-- end container-fluid -->
+
+
+
+
+    <!-- JAVASCRIPT -->
+    <script src="{{ asset('libs/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+    <script src="{{ asset('js/pages/dashboard.init.js') }}"></script>
+
+    <script src="{{ asset('js/app.js') }}"></script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
+    <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
+
+    <script type="text/javascript">
+    $(function() {
+        $("#btn1").click(function() {
+            $("#form1").show();
+            $("#form2").hide();
+        });
+        $("#btn2").click(function() {
+            $("#form1").hide();
+            $("#form2").show();
+        });
+        $("#link1").click(function() {
+            $("#cont1").show();
+            $("#cont2").hide();
+            $("#cont3").hide();
+            $("#cont4").hide();
+        });
+        $("#link2").click(function() {
+            $("#cont1").hide();
+            $("#cont2").show();
+            $("#cont3").hide();
+            $("#cont4").hide();
+        });
+        $("#link3").click(function() {
+            $("#cont1").hide();
+            $("#cont2").hide();
+            $("#cont3").show();
+            $("#cont4").hide();
+        });
+        $("#link4").click(function() {
+            $("#cont1").hide();
+            $("#cont2").hide();
+            $("#cont3").hide();
+            $("#cont4").show();
+        });
+    });
+    </script>
+
+</body>
+
+</html>
